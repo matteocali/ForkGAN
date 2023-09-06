ae8e51a9141c1d4d42b72caf420a022dacac7a07
Matteo Caligiuri <matteo.caligiuri@dei.unipd.it>
2023-09-06 10:49:15 +0200

    Update launch script and removed unused ones

--- scripts/visdrone_test.sh
+++ scripts/visdrone_test.sh
@@ -1,2 +1,12 @@
 ### test
-python main.py --phase test --dataset_dir visdrone --gpu 0 --n_d 2 --n_scale 2 --checkpoint_dir ./check/visdrone --test_dir ./check/visdrone/testa2b --which_direction AtoB
+python main.py \
+    --phase test \
+    --dataset_dir visdrone \
+    --gpu 0 \
+    --n_d 2 \
+    --n_scale 2 \
+    --which_direction BtoA \
+    --checkpoint_dir ./check/visdrone \
+    --sample_dir ./check/visdrone/sample \
+    --test_dir ./check/visdrone/testB2A
+    
--- scripts/visdrone_train.sh
+++ scripts/visdrone_train.sh
@@ -1,2 +1,13 @@
 ### train
-python main.py --phase train --dataset_dir visdrone --epoch 20 --gpu 0 --n_d 2 --n_scale 2 --which_direction BtoA --checkpoint_dir ./check/visdrone --sample_dir ./check/visdrone/sample --test_dir ./check/visdrone/testa2b --L1_lambda 10
+python main.py \
+    --phase train \
+    --dataset_dir visdrone \
+    --epoch 20 \
+    --gpu 0 \
+    --n_d 2 \
+    --n_scale 2 \
+    --L1_lambda 10 \
+    --which_direction BtoA \
+    --checkpoint_dir ./check/visdrone \
+    --sample_dir ./check/visdrone/sample \
+    --test_dir ./check/visdrone/testB2A
